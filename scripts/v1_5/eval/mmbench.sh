c160d482b1a1e89cdbfb688d2e467ac4e880ddc2
Zichen Wen <Zichen.Wen@outlook.com>
2025-02-18 14:09:26 +0800

    initial commit

--- scripts/v1_5/eval/mmbench.sh
+++ scripts/v1_5/eval/mmbench.sh
@@ -1,19 +1,32 @@
 #!/bin/bash
 
+export CUDA_VISIBLE_DEVICES=0
+
+CKPT=YOUR_MODEL_PATH
+MODEL=llava-v1.5-7b
+
 SPLIT="mmbench_dev_20230712"
 
 python -m llava.eval.model_vqa_mmbench \
-    --model-path liuhaotian/llava-v1.5-13b \
+    --model-path $CKPT \
     --question-file ./playground/data/eval/mmbench/$SPLIT.tsv \
-    --answers-file ./playground/data/eval/mmbench/answers/$SPLIT/llava-v1.5-13b.jsonl \
+    --answers-file ./playground/data/eval/mmbench/answers/$SPLIT/$MODEL.jsonl \
     --single-pred-prompt \
     --temperature 0 \
-    --conv-mode vicuna_v1
+    --conv-mode vicuna_v1 \
+    --sparse \
+    --attn_implementation sdpa \
+    --pruned_layer 2 \
+    --image_token_start_index 35 \
+    --image_token_length 576 \
+    --reduction_ratio 0.778 \
+    --pivot_image_token 4 \
+    --pivot_text_token 4 
 
-mkdir -p playground/data/eval/mmbench/answers_upload/$SPLIT
+mkdir -p ./playground/data/eval/mmbench/answers_upload/$SPLIT
 
 python scripts/convert_mmbench_for_submission.py \
     --annotation-file ./playground/data/eval/mmbench/$SPLIT.tsv \
     --result-dir ./playground/data/eval/mmbench/answers/$SPLIT \
     --upload-dir ./playground/data/eval/mmbench/answers_upload/$SPLIT \
-    --experiment llava-v1.5-13b
+    --experiment $MODEL
